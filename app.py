03ad7d4b3282c86749754d3f10ef6f2a38bcf26e
Sarthak Singh <105309861+sarthakk1890@users.noreply.github.com>
2022-12-19 19:50:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-!pip install scikit-learn
 
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
